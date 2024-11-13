56a419c82d4917644d750fa12ca495731c8d5da6
Vicente <vicenteferrer_01@hotmail.com>
2024-11-13 19:38:32 -0300

    Template Implementation

--- src/js/ProductDetails.mjs
+++ src/js/ProductDetails.mjs
@@ -10,7 +10,12 @@ export default class ProductDetails {
     async init() {
         this.product = await this.dataSource.findProductById(this.productId);
 
-        return this;
+        this.renderProductDetails();
+
+        // Add event listener using Function.prototype.bind() method
+        // which returns a new function with current instance settled as
+        // 'this' inner function context.
+        qs("#addToCart").addEventListener("click", this.addToCart.bind(this));
     }
 
     addToCart() {
@@ -24,36 +29,46 @@ export default class ProductDetails {
     }
 
     renderProductDetails() {
+        // Set title for custom product page
+        qs("title", document.head).innerHTML =
+            `Sleep Outside | ${this.product.Name}`;
+
+        // Get product detail node
         const productDetailElement = qs(".product-detail");
+        // Get product detail template content
+        const productTemplate = qs("#product-template");
 
-        productDetailElement.innerHTML = `<h3>${this.product.Brand.Name}</h3>
+        // Clone template content
+        const clone = productTemplate.content.cloneNode(true);
 
-                <h2 class="divider">${this.product.NameWithoutBrand}</h2>
+        // Locate template pieces to customize content
+        const brandH3 = qs("h3", clone);
+        const nameWBH2 = qs("h2", clone);
+        const productImg = qs("img", clone);
+        const pricePara = qs(".product-card__price", clone);
+        const colorPara = qs(".product__color", clone);
+        const descriptionPara = qs(".product__description", clone);
+        const addButton = qs("#addToCart", clone);
 
-                <img
-                    class="divider"
-                    src="${this.product.Image}"
-                    alt="${this.product.Name}"
-                />
+        // Set custom comtent based on product requested
+        brandH3.textContent = this.product.Brand.Name;
+        nameWBH2.textContent = this.product.NameWithoutBrand;
+        productImg.setAttribute("src", this.product.Image);
+        productImg.setAttribute("alt", this.product.Name);
+        pricePara.textContent = this.product.ListPrice;
+        colorPara.textContent = this.product.Colors.reduce(
+            (acc, color, i, arr) => {
+                acc += color.ColorName;
+                if (i > arr.length - 1) acc += ", ";
 
-                <p class="product-card__price">${this.product.ListPrice}</p>
+                return acc;
+            },
+            "",
+        );
+        descriptionPara.innerHTML = this.product.DescriptionHtmlSimple;
+        addButton.setAttribute("data-id", this.product.Id);
 
-                <p class="product__color">${this.product.Colors.reduce(
-                    (acc, color, i, arr) => {
-                        acc += color.ColorName;
-                        if (i > arr.length - 1) acc += ", ";
-
-                        return acc;
-                    },
-                    "",
-                )}</p>
-
-                <p class="product__description">
-                    ${this.product.DescriptionHtmlSimple}
-                </p>
-
-                <div class="product-detail__add">
-                    <button id="addToCart" data-id="${this.product.Id}">Add to Cart</button>
-                </div>`;
+        // Append customized template to main view
+        productDetailElement.appendChild(clone);
     }
 }
--- src/js/product.js
+++ src/js/product.js
@@ -7,14 +7,4 @@ const dataSource = new ProductData("tents");
 
 const product = new ProductDetails(productId, dataSource);
 
-product.init().then((prod) => prod.renderProductDetails());
-
-// add to cart button event handler
-async function addToCartHandler() {
-    product.addToCart();
-}
-
-// add listener to Add to Cart button
-document
-    .getElementById("addToCart")
-    .addEventListener("click", addToCartHandler);
+product.init();
