7fa0ae695ba6f28df5be8ed9ca5c439941392e4e
Phoebe <hsingyinlin@gmail.com>
2024-11-25 23:51:40 +0800

    W03 Individual Activity: Expand Inventory

--- src/js/product-listing.js
+++ src/js/product-listing.js
@@ -2,7 +2,7 @@ import { qs } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 import ProductListing from "./ProductList.mjs";
 
-const dataSource = new ProductData("tents");
+const dataSource = new ProductData("/publicjson/tents");
 const productList = new ProductListing(
     "tents",
     dataSource,
--- vite.config.js
+++ vite.config.js
@@ -12,6 +12,8 @@ export default defineConfig({
                 cart: resolve(__dirname, "src/cart/index.html"),
                 checkout: resolve(__dirname, "src/checkout/index.html"),
                 product: resolve(__dirname, "src/product_pages/index.html"),
+                productlisting: resolve(__dirname, "src/product_listing/index.html"),
+                newPage: resolve(__dirname, "src/product_listing/newPage.html"),
             },
         },
     },
